2ffee8ec111176816ebcf53d85a3d03a85de6344
kenshinhm <kenshinhm@naver.com>
2019-04-28 23:52:48 +0900

    install app images & remove unneccesary files

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -73,6 +73,7 @@ THIRD_PARTY_APPS = [
 ]
 LOCAL_APPS = [
     "instaclone.users.apps.UsersAppConfig",
+    "instaclone.images.apps.ImagesConfig",
     # Your stuff: custom apps go here
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
