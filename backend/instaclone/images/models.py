ad44922996e3df1c2106cc049a437bcbcff237ca
kenshinhm <kenshinhm@naver.com>
2019-04-29 21:39:24 +0900

    add images model

--- backend/instaclone/images/models.py
+++ backend/instaclone/images/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+
+class TimestampModel(models.Model):
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Image(TimestampModel):
+
+    file = models.ImageField()
+    location = models.CharField(max_length=140)
+    caption = models.TextField()
+
+
+class Comment(TimestampModel):
+
+    message = models.TextField()
