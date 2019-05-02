24e92003f5dd2a5ef9d75d013ab238c5490ff13f
jake <kenshinhm@naver.com>
2019-05-02 18:51:44 +0900

    setting up JWT

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,15 +1,15 @@
 from django.conf import settings
+from django.conf.urls import url
 from django.urls import include, path
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.views.generic import TemplateView
 from django.views import defaults as default_views
+from rest_framework_jwt.views import obtain_jwt_token
 
 urlpatterns = [
     path("", TemplateView.as_view(template_name="pages/home.html"), name="home"),
-    path(
-        "about/", TemplateView.as_view(template_name="pages/about.html"), name="about"
-    ),
+    path("about/", TemplateView.as_view(template_name="pages/about.html"), name="about"),
     # Django Admin, use {% url 'admin:index' %}
     path(settings.ADMIN_URL, admin.site.urls),
     # User management
@@ -18,6 +18,7 @@ urlpatterns = [
     # Your stuff: custom urls includes go here
     path("images/", include("instaclone.images.urls", namespace="images")),
     path("notifications/", include("instaclone.notifications.urls", namespace="notifications")),
+    url(r'^api-token-auth/', obtain_jwt_token),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
