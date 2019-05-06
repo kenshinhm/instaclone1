73657d98a576f572d366f1b07cf36b5b9332604a
jake <kenshinhm@naver.com>
2019-05-06 12:15:26 +0900

    improving the resposive mixin

--- frontend/src/components/AuthForms/index.js
+++ frontend/src/components/AuthForms/index.js
@@ -6,18 +6,13 @@ export const LoginForm = props => (
     <div className={styles.formComponent}>
         <form className={styles.form}>
             <input type="text" placeholder="Username" className={styles.textInput}/>
-            <input
-                type="password"
-                placeholder="Password"
-                className={styles.textInput}
-            />
+            <input type="password" placeholder="Password" className={styles.textInput}/>
             <input type="submit" value="Log in" className={styles.button}/>
         </form>
         <span className={styles.divider}>or</span>
         <span className={styles.facebookLink}>
-      <Ionicon icon="logo-facebook" fontSize="20px" color="#385185"/> Log in
-      with Facebook
-    </span>
+            <Ionicon icon="logo-facebook" fontSize="20px" color="#385185"/> Log in with Facebook
+        </span>
         <span className={styles.forgotLink}>Forgot password?</span>
     </div>
 );
