0ee954cbd69444360058928914e22e1397e9597e
jake <kenshinhm@naver.com>
2019-05-06 13:06:49 +0900

    stateless components and context

--- frontend/src/components/Footer/index.js
+++ frontend/src/components/Footer/index.js
@@ -2,38 +2,33 @@ import React from 'react';
 import styles from './styles.scss';
 import PropTypes from 'prop-types';
 
-class Footer extends React.Component {
+const Footer = (props, context) => (
+    <footer className={styles.footer}>
+        <div className={styles.column}>
+            <nav className={styles.nav}>
+                <ul className={styles.list}>
+                    <li className={styles.listItem}>{context.t("About Us")}</li>
+                    <li className={styles.listItem}>{context.t("Support")}</li>
+                    <li className={styles.listItem}>{context.t("Blog")} </li>
+                    <li className={styles.listItem}>{context.t("Press")} </li>
+                    <li className={styles.listItem}>{context.t("API")} </li>
+                    <li className={styles.listItem}>{context.t("Jobs")} </li>
+                    <li className={styles.listItem}>{context.t("Privacy")} </li>
+                    <li className={styles.listItem}>{context.t("Terms")} </li>
+                    <li className={styles.listItem}>{context.t("Directory")} </li>
+                    <li className={styles.listItem}>{context.t("Language")} </li>
+                </ul>
+            </nav>
+        </div>
+        <div className={styles.column}>
+            <span className={styles.copyright}> © 2019 instaclone</span>
+        </div>
 
-    // static contextTypes = {
-    //     t: PropTypes.func.isRequired
-    // };
+    </footer>
+);
 
-    render() {
-        return (
-            <footer className={styles.footer}>
-                <div className={styles.column}>
-                    <nav className={styles.nav}>
-                        <ul className={styles.list}>
-                            <li className={styles.listItem}>{this.context.t("About Us")}</li>
-                            <li className={styles.listItem}>Support</li>
-                            <li className={styles.listItem}>Blog</li>
-                            <li className={styles.listItem}>Press</li>
-                            <li className={styles.listItem}>API</li>
-                            <li className={styles.listItem}>Jobs</li>
-                            <li className={styles.listItem}>Privacy</li>
-                            <li className={styles.listItem}>Terms</li>
-                            <li className={styles.listItem}>Directory</li>
-                            <li className={styles.listItem}>Language</li>
-                        </ul>
-                    </nav>
-                </div>
-                <div className={styles.column}>
-                    <span className={styles.copyright}> © 2019 instaclone</span>
-                </div>
-
-            </footer>
-        );
-    }
-}
+Footer.contextTypes = {
+    t: PropTypes.func.isRequired
+};
 
 export default Footer;
