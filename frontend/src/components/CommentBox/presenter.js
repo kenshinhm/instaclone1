4984af3911cf85a0631f9fbd3cecd360532ffecb
jake <kenshinhm@naver.com>
2019-05-10 23:54:11 +0900

    responsive feed + making textarea fit content

--- frontend/src/components/CommentBox/presenter.js
+++ frontend/src/components/CommentBox/presenter.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import styles from './styles.scss';
 import PropTypes from "prop-types";
+import Textarea from "react-textarea-autosize";
 
 const CommentBox = (props, context) => (
     <form>
-        <textarea placeholder={context.t("Add a comment..")}>
+        <Textarea placeholder={context.t("Add a comment..")}>
 
-        </textarea>
+        </Textarea>
     </form>
 );
 
