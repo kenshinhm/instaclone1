3f6d64db64d433ffec367108ef04f04958a5b57d
jake <kenshinhm@naver.com>
2019-05-12 18:42:53 +0900

    search part 1

--- frontend/src/components/Navigation/container.js
+++ frontend/src/components/Navigation/container.js
@@ -3,12 +3,38 @@ import Navigation from "./presenter";
 import PropTypes from "prop-types";
 
 class Container extends React.Component {
+    static propTypes = {
+        goToSearch: PropTypes.func.isRequired,
+    };
+
+    state = {
+        term: ""
+    };
 
     render() {
         return (
-            <Navigation/>
+            <Navigation value={this.state.term}
+                        onSubmit={this._onSubmit}
+                        onInputChange={this._onInputChange}/>
         );
     }
+
+    _onInputChange = (event) => {
+        const {target: {value}} = event;
+        this.setState({
+            term: value
+        });
+    };
+
+    _onSubmit = (event) => {
+        const {goToSearch} = this.props;
+        const {term} = this.state;
+        event.preventDefault();
+        this.setState({
+            term: ""
+        });
+        goToSearch(term);
+    };
 }
 
 export default Container;
